75beb2ed4c9c769231cb426a1061ade9164febef
Bia Luz <anabluz08@gmail.com>
2023-09-21 02:05:18 -0300

    aula-01-prisma

---
+++ aula-prisma-01/src/protocols/post-protocol.ts
@@ -0,0 +1,14 @@
+export type Post = {
+  id: number;
+  username: string;
+  title: string;
+  content: string;
+  createdAt?: Date;
+}
+
+export type ApplicationError = {
+  name: string;
+  message: string;
+};
+
+
